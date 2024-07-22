9b1314101fd65df28af4a84c161362e8381fff7c
Ritik Sharma <146114547+ritiksharma2204@users.noreply.github.com>
2024-07-22 16:23:58 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) // we r using nodejs, not browser
+
+console.log(3+3); 
+
+console.log("ritik")
+
+let name = "ritik"  //string data type
+let age = 18     //number 
+let isLoggedIn = false  //boolean 
+
+
+//number => 2 to power 53
+// bigint
+//string =>""
+//boolean => true/false
+//null => standalone value
+// undefined => 
+//symbol => unique
+
+
+// object
+
+console.log(typeof undefined)// output=> undefined
+console.log(typeof null) // output=> object
